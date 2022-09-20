fe07464f93fea5734beae86c5419d3e76ba1c014
Zach Fechko <zachary.fechko@wsu.edu>
2022-09-20 16:34:24 -0700

    worked on PA 1 a bit

--- Assignments/PA 1/main.py
+++ Assignments/PA 1/main.py
@@ -5,5 +5,45 @@ PA 1
 """
 
 from apyori import apriori
-import csv
-import pandas as pd
+from itertools import combinations, chain
+
+INFILE = 'data/browsing_data.txt'
+OUTFILE = 'output.txt'
+SUPPORT = 100
+
+def read_infile():
+    """
+    reads in the input file and returns groomed data in a list of lists
+    """
+    with open(INFILE, 'r') as infile:
+        all_lines = infile.readlines()
+
+    return [[data_point for data_point in line.split()] for line in all_lines]
+
+def apriori_pass_1(dataset, support):
+    """
+    finds the singles in the dataset
+    """
+    item_count = {}
+    for basket in dataset:
+        for item in basket:
+            if item not in item_count:
+                item_count[item] = 1
+            else:
+                item_count[item] += 1
+    return {key: value for key, value in item_count.items() if value >= support}
+
+def apriori_pass_2(dataset, support, singles):
+    """
+    finds the doubles in the dataset
+    """
+    double_count = {}
+    for basket in dataset:
+        for item in combinations(basket, 2):
+            if item not in double_count:
+                double_count[item] = 1
+            else:
+                double_count[item] += 1
+    return {key: value for key, value in double_count.items() if value >= support}
+    
+
