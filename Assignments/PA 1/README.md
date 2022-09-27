fb3cf8b9c155184a95282881d6947093f2dbd411
Zach Fechko <zachary.fechko@wsu.edu>
2022-09-26 18:08:20 -0700

    finished PA 1

--- Assignments/PA 1/README.md
+++ Assignments/PA 1/README.md
@@ -3,8 +3,8 @@
 ## Instructions
 
 ### Requirements
-- [ ] include a 'readme.txt' file on full instructions to run your program, within this folder. A list of all libraries used should also be included
-- [ ] This folder should have a script file named run_code.sh
+- [x] include a 'readme.txt' file on full instructions to run your program, within this folder. A list of all libraries used should also be included
+- [x] This folder should have a script file named run_code.sh
 - Assume relative paths
 - Submit as a zip file
 
@@ -13,28 +13,28 @@ Suppose we want to recommend new products to the customer based on the products 
 
 Use the online browsing behavior dataset provided. Each line represents a customer's browsing session. On each line, each string of characters represents the ID of an item browsed during that session. The items are separated by spaces.
 
-- Identify pairs of items $(X,Y)$ such that the support of $\{X,Y\}$ is at least 100. For all such pairs, compute the confidence scores of the corresponding association rules $X\rightarrow Y, Y\rightarrow X$. Sort the rules in decreasing order of confidence scores and list the top 5 rules in the writeup. Break ties, if any, by lexicographically increasing order on the left hand side of the rule
+- Identify pairs of items $(X,Y)$ such that the support of $\{X,Y\}$ is at least 100. For all such pairs, compute the confidence scores of the corresponding association rules $X\implies Y, Y\implies X$. Sort the rules in decreasing order of confidence scores and list the top 5 rules in the writeup. Break ties, if any, by lexicographically increasing order on the left hand side of the rule
 - Identify item triples $(X, Y, Z)$ such that the support of $\{X, Y, Z\}$ is at 
 least 100. For all such triples, compute the confidence scores of the 
-corresponding association rules: $(X, Y) \rightarrow Z, (X, Z) \rightarrow Y, (Y, Z) \rightarrow X$. 
+corresponding association rules: $(X, Y) \implies Z, (X, Z) \implies Y, (Y, Z) \implies X$. 
 Sort the rules in decreasing order of confidence scores and list the top 5 
 rules in the writeup. Order the left-hand-side pair lexicographically and 
 break ties, if any, by lexicographical order of the first then the second 
 item in the pair. 
 - The output of your program should be dumped in a file named "output.txt" in the following format:
 ```
-OUTPUT A 
-FRO11987 FRO12685 0.4325 
-FRO11987 ELE11375 0.4225 
-FRO11987 GRO94758 0.4125 
-FRO11987 SNA80192 0.4025 
-FRO11987 FRO18919 0.4015 
-OUTPUT B 
-FRO11987 FRO12685 DAI95741 0.4325 
-FRO11987 ELE11375 GRO73461 0.4225 
-FRO11987 GRO94758 ELE26917 0.4125 
-FRO11987 SNA80192 ELE28189 0.4025 
-FRO11987 FRO18919 GRO68850 0.4015 
+OUTPUT A
+DAI93865 FRO40251 1.0
+GRO85051 FRO40251 0.999176276771005
+GRO38636 FRO40251 0.9906542056074766
+ELE12951 FRO40251 0.9905660377358491
+DAI88079 FRO40251 0.9867256637168141
+OUTPUT B
+DAI23334 ELE92920 DAI62779 1.0
+DAI31081 GRO85051 FRO40251 1.0
+DAI55911 GRO85051 FRO40251 1.0
+DAI62779 DAI88079 FRO40251 1.0
+DAI75645 GRO85051 FRO40251 1.0
 ```
 - Output breakdown:
     - Line 1 should have "Output A"
--- Assignments/PA 1/main.py
+++ Assignments/PA 1/main.py
@@ -2,6 +2,7 @@
 Zach Fechko
 Comp Sci 315
 PA 1
+Note: If there's any similarity to anthony ghimpu's code, we helped each other out a bit
 """
 
 #from apyori import apriori
@@ -35,87 +36,146 @@ def apriori_pass_1(dataset, support) -> dict:
             else:
                 item_count[item] += 1
         line_count += 1
-        bar.update(line_count / len(dataset) * 100)
+        bar.update((line_count / len(dataset)) * 100)
     bar.finish()
     return {key: value for key, value in item_count.items() if value >= support}
 
 def apriori_pass_2(dataset, singles, support) -> dict:
     """
-    finds the frequent pairs in the dataset
+    - finds the frequent pairs in the dataset. 
+    - Returns a dictionary of pairs and their support
     """
     item_counts = {} #dictionary of item counts
     line_count = 0
     bar = pb.ProgressBar().start()
-    combos = set(combinations(singles, 2)) #set of tuples that have all possible combinations of 2 items
     for line in dataset:
+        temp = set(line).intersection(singles) #only look at the frequent singles
+        combos = list(map(frozenset, combinations(temp, 2))) #get all combinations of size 2 from temp
         for combination in combos: #for each combination
-            if combination[0] in line and combination[1] in line: #if both items are in the same line
-                if combination not in item_counts: #if the combination is not in the dictionary
-                    item_counts[combination] = 1 #add it to the dictionary and set its value to 1
-                else:
-                    item_counts[combination] += 1 #if it is in the dictionary, increment its value
+            if item_counts.get(combination): #if the combination is already in the dictionary
+                item_counts[combination] += 1 #increment the count
+            else:
+                item_counts[combination] = 1 #otherwise, add the combination to the dictionary
         line_count += 1
-        bar.update(line_count / len(dataset) * 100)
+        bar.update((line_count / len(dataset)) * 100) #update the progress bar based on the number of lines processed
     bar.finish()
     return {key: value for key, value in item_counts.items() if value >= support} #return the dictionary with only the items that meet the support
 
 
 def apriori_pass_3(dataset, support, singles) -> dict:
     """
-    finds the frequent triples in the dataset
+    - finds the frequent triples in the dataset
+    - returns a dictionary of triples and their support
     """
     item_counts = {} #dictionary of item counts
     line_count = 0
     bar = pb.ProgressBar().start() #progress bar
-    combos = set(combinations(singles, 3)) #set of tuples that have all possible combinations of 3 items
     for line in dataset:
-        for combination in combos:
-            if combination[0] in line and combination[1] in line and combination[2] in line:
-                if combination not in item_counts:
-                    item_counts[combination] = 1
-                else:
-                    item_counts[combination] += 1
+        temp = set(line).intersection(singles) #only look at the items that are frequent singles
+        combos = list(map(frozenset, combinations(temp, 3))) #get all combinations of size 3 from temp
+        for combination in combos: #for each combination
+            if item_counts.get(combination): #if the combination is already in the dictionary
+                item_counts[combination] += 1 #increment the count
+            else:
+                item_counts[combination] = 1 #otherwise, add the combination to the dictionary
         line_count += 1
-        bar.update(line_count / len(dataset) * 100) #update the progress bar based on the number of lines processed
+        bar.update((line_count / len(dataset)) * 100) #update the progress bar based on the number of lines processed
     bar.finish() #finish the progress bar
     return {key: value for key, value in item_counts.items() if value >= support}
 
 def calculate_doubles_confidence(frequent_pairs, frequent_singles):
     """
-    calculates the confidence for each pair
+    - calculates the confidence of the association rules for each pair, which is calculated as
+    number of times the pair occurs / number of times the first item occurs. 
+    - Returns a list of tuples sorted by confidence in descending order
     """
+    confidence = {}
+    for pair in list(map(tuple, frequent_pairs.keys())):
+        #X => Y
+        confidence[(pair[0], pair[1])] = frequent_pairs[frozenset(pair)] / frequent_singles[pair[0]]
+        #Y => X
+        confidence[(pair[1], pair[0])] = frequent_pairs[frozenset(pair)] / frequent_singles[pair[1]]
+    return sorted(confidence.items(), key=lambda item: item[1], reverse=True) #return sorted confidence list
 
-def calculate_triples_confidence(frequent_triples, frequent_pairs, frequent_singles):
-    """
-    calculates the confidence for each triple
-    """
+    
+    
 
-def dump_to_file(pairs_output, triples_output):
+def calculate_triples_confidence(frequent_triples, frequent_pairs):
+    """
+    - calculates the confidence for each triple
+    - returns a list of tuples sorted by confidence in descending order
+    """
+    confidence = {}
+    for triple in list(map(tuple, frequent_triples.keys())):
+        #(X, Y) => Z
+        confidence[(triple[0], triple[1], triple[2])] = frequent_triples[frozenset(triple)] / frequent_pairs[frozenset((triple[0], triple[1]))]
+        #(X, Z) => Y
+        confidence[(triple[0], triple[2], triple[1])] = frequent_triples[frozenset(triple)] / frequent_pairs[frozenset((triple[0], triple[2]))]
+        #(Y, Z) => X
+        confidence[(triple[1], triple[2], triple[0])] = frequent_triples[frozenset(triple)] / frequent_pairs[frozenset((triple[1], triple[2]))]
+    return sorted(confidence.items(), key=lambda item: item[1], reverse=True)
+
+def print_doubles_confidence(doubles_confidence):
+    """
+    Prints the pairs and their confidence the same way we would for dumping to the txt file
+    """
+    print("Output A:")
+    for pair in doubles_confidence:
+        print(f"{pair[0][0]} {pair[0][1]} {pair[1]}\n")
+
+def print_triples_confidence(triples_confidence):
+    """
+    Prints the triples and their confidence the same way we would for dumping to the txt file
+    """
+    print("Output B:")
+    for triple in triples_confidence:
+        print(f"{triple[0][0]} {triple[0][1]} {triple[0][2]} {triple[1]}\n")
+
+def dump_to_file(pairs_output: list[tuple], triples_output: list[tuple]):
     """
     dumps the output to a file following the format in the assignment description
     """
     with open(OUTFILE, 'w') as outfile:
         #write the pairs + associated confidence
         outfile.write("Output A:\n")
+        for pair in pairs_output:
+            outfile.write(f"{pair[0][0]} {pair[0][1]} {pair[1]}\n")
+        #write the triples + associated confidence
+        outfile.write("\nOutput B:\n")
+        for triple in triples_output:
+            outfile.write(f"{triple[0][0]} {triple[0][1]} {triple[0][2]} {triple[1]}\n")
         
 
 def main():
     """
     Carries out assignment flow
     - reads in the data
-    - finds the pairs that fit the support
-    - finds the triples that fit the support
+    - finds the pairs that fit the support s = 100
+    - finds the triples that fit the support s = 100
     - calculates confidence value for each pair/triple
     - dumps output to file
     """
     groomed_data = read_infile()
+
     print("finding singles")
     support_singles = apriori_pass_1(groomed_data, SUPPORT)
-    singles = list(support_singles.keys())
+    singles = set(support_singles.keys())
+
     print("finding pairs")
     support_pairs = apriori_pass_2(groomed_data, singles, SUPPORT)
+    
     frequent_items = set(chain.from_iterable(support_pairs.keys()))
-    print(frequent_items)
+
+    print("finding triples")
+    support_triples = apriori_pass_3(groomed_data, SUPPORT, frequent_items)
+
+    pairs_confidence = calculate_doubles_confidence(support_pairs, support_singles)
+    triples_confidence = calculate_triples_confidence(support_triples, support_pairs)
+    
+    print_doubles_confidence(pairs_confidence[:5])
+    print_triples_confidence(triples_confidence[:5])
+
+    dump_to_file(pairs_confidence[:5], triples_confidence[:5])
 
     
 if __name__ == '__main__':
